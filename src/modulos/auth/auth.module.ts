620c393a078ce6fd238bc27b12a0f31b0e96dc82
morgadodesarrollador <morgadodesarrollador@gmail.com>
2022-12-19 09:50:13 +0100

    apiIncidencias

--- src/modulos/auth/auth.module.ts
+++ src/modulos/auth/auth.module.ts
@@ -6,29 +6,33 @@ import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtStrategy } from './strategies/jwt.strategy';
+//import { JwtStrategy } from './strategies/jwt.strategy';
+
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, JwtStrategy],
+ // providers: [AuthService, JwtStrategy],
+  providers: [AuthService ],
+  
   imports: [
     ConfigModule,
     TypeOrmModule.forFeature([ User ]),
-    PassportModule.register( { defaultStrategy: 'jwt'}),
-    JwtModule.registerAsync({
-      imports: [ ConfigModule ],
-      inject: [ ConfigService ],
-      useFactory: ( configService: ConfigService) => {
-        return {
-          secret: configService.get('JWT_SECRET'),
-          signOptions:{
-            expiresIn: '2h'
-          }
-        }
-      }
-    })
+    // PassportModule.register( { defaultStrategy: 'jwt'}),
+    // JwtModule.registerAsync({
+    //   imports: [ ConfigModule ],
+    //   inject: [ ConfigService ],
+    //   useFactory: ( configService: ConfigService) => {
+    //     return {
+    //       secret: configService.get('JWT_SECRET'),
+    //       signOptions:{
+    //         expiresIn: '2h'
+    //       }
+    //     }
+    //   }
+    // })
   ],
-  exports: [ TypeOrmModule, AuthService, JwtStrategy, PassportModule, JwtModule ]
+//  exports: [ TypeOrmModule, AuthService, JwtStrategy, PassportModule, JwtModule ]
+  exports: [ TypeOrmModule ]
 
 })
 export class AuthModule {}
